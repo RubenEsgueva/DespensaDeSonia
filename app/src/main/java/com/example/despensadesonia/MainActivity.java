ce9b53f04f40250078ae4227192531eb41354899
RubenEsgueva <resgueva94@gmail.com>
2024-03-16 14:12:16 +0100

    Esqueleto inicial

--- app/src/main/java/com/example/despensadesonia/MainActivity.java
+++ app/src/main/java/com/example/despensadesonia/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.despensadesonia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //FIJAMOS UN IDIOMA AL INICIAR LA APLICACIÓN
+        cambiarIdioma();
+        //LANZAMOS EL LOGIN PARA QUE PUEDA EMPEZAR A USAR LA APP EL USUARIO
+        Intent i = new Intent(this, LoginActivity.class);
+        startActivity(i);
+    }
+
+    private void cambiarIdioma(){
+        //EL IDIOMA BASE ES CASTELLANO, MÁS ADELANTE SE PODRÁ CAMBIAR
+        Locale nuevaloc = new Locale("es");
+        Locale.setDefault(nuevaloc);
+        Configuration configuration =
+                getBaseContext().getResources().getConfiguration();
+        configuration.setLocale(nuevaloc);
+        configuration.setLayoutDirection(nuevaloc);
+
+        Context context =
+                getBaseContext().createConfigurationContext(configuration);
+        getBaseContext().getResources().updateConfiguration(
+                configuration, context.getResources().getDisplayMetrics());
     }
 }
